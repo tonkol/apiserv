f17532804086f4fd41131d13e2e293e86ca9935b
= <=>
2016-09-08 10:57:00 +0300

    Add task and task db model

--- api/views.py
+++ api/views.py
@@ -1,5 +1,27 @@
 from apiserv import app
 
-@app.route('/')
-def index():
-    return "api handler for root"
+# Import models for task
+from task.models import Task
+
+# Not very elegant but works
+def define_json_errorhandler(error_code):
+    # Check that there isn't any existing handlers defined    
+    if type(app.error_handlers) is dict:        
+        if error_code not in app.error_handlers:
+            app.logger.debug("API.views: Registering errorhandler for code %d" % error_code)
+            @app.errorhandler(error_code)
+            def handle_error(error):
+                # import pdb; pdb.set_trace()
+                err = {
+                    'message': error.description,
+                    'error_code': error.code
+                }
+                return jsonify(err)
+        else:
+            app.logger.debug("API.views: Handler for error code %d is already registered." % error_code)
+
+
+# Define some generic error handlers if missing
+error_codes = [404, 405]
+for error_code in error_codes:
+    define_json_errorhandler(error_code)
---
+++ task/models.py
@@ -0,0 +1,37 @@
+from apiserv import db
+
+"""
+Database models for Flask-SQLAlchemy
+"""
+
+"""
+Task DB Model
+"""
+class Task(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    creation_date = db.Column(db.DateTime)
+    completion_date = db.Column(db.DateTime)
+    # owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # category = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    def __init__(
+        self,
+        name,
+        # owner, 
+        body=None,
+        creation_date=None,
+        completion_date=None,
+        # category=None
+    ):
+        
+        self.name = name
+        # self.owner = owner
+        self.body = body
+        self.creation_date = creation_date
+        self.completion_date = completion_date
+        # self.category = category
+
+    def __repr__(self):
+        return '<Task %r>' % self.name
